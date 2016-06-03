46a57be94356b65bb36c94dac440f7046a4d010b
Cathy Cushy <hakuna.matata.kitty@gmail.com>
2016-06-03 14:59:05 -0500

    TODO fix nested promise

---
+++ Chat/scripts/signup.js
@@ -0,0 +1,69 @@
+'use strict'
+
+function SignUp() {
+    this.submitForm = document.getElementById('submitForm');
+    this.submitButton = document.getElementById('submitButton');
+    this.emailInput = document.getElementById('email-input');
+    this.locationInput = document.getElementById('location-input');
+    this.passwordInput = document.getElementById('password-input');
+
+    this.submitForm.addEventListener('submit', this.submit.bind(this));
+
+
+    this.initFirebase();
+}
+
+SignUp.prototype.initFirebase = function() {
+    this.auth = firebase.auth();
+    this.database = firebase.database();
+    this.storage = firebase.storage();
+    console.log('initFirebase');
+    alert('init' + firebase.database());
+    // this.auth.onAuthStateChanged(this.onAuthStateChanged.bind(this));
+};
+
+SignUp.prototype.submit = function(e) {
+    e.preventDefault();
+    var enteredEmail = this.emailInput.value;
+    var enteredLocation = this.locationInput.value;
+    var enteredPassword = this.passwordInput.value;
+
+    // Required, will redirect
+    // TODO: Simplify this nested promise
+    if(enteredEmail && enteredPassword){
+        this.auth.createUserWithEmailAndPassword(enteredEmail, enteredPassword)
+            .then(function(user){
+                console.log(this);
+                console.log(this.database);
+                firebase.usersRef = firebase.database().ref('users');
+                firebase.usersRef.set(
+                    {
+                        uid: user.uid,
+                        username: user.displayName || "Unnamed User",
+                        photoURL: user.photoURL || "./images/profile_placeholder.jpg",
+                        email: enteredEmail,
+                        // Do not pass in password for security reasons
+                        location: enteredLocation,
+                    }
+                ).then(function(snapshot) {
+                    alert(snapshot.val());
+                    window.location = "./";
+                }.bind(this)).catch(function(err){
+                    console.error('Error creating user ', err);
+                    alert('Sorry, this server error occurred:\n' + err);
+                })
+            })
+            .catch(function(error) {
+                var errorCode = error.code;
+                var errorMessage = error.message;
+                console.log(errorCode, errorMessage);
+                alert('Sorry, this creation error occurred\n' + errorCode + "\n" + errorMessage + "\n" +
+                        "If you could kindly contact us with your web browser and any details, we will do our best to fix it. Thanks");
+            });
+    }
+
+};
+
+window.onload = function() {
+    window.SignUp = new SignUp();
+};
